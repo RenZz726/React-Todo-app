e54f6559b34a56fe372e149b22316891e108e6fd
RenZz726 <rensonkroji02@gmail.com>
2025-12-19 11:10:31 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,28 @@
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, toDos, setToDos }) {
+  function handleClick(item) {
+    setToDos(toDos.map((todo) =>
+      todo.name === item ? { ...todo, done: !todo.done } : todo
+    ));
+  }
+  function handleDelete(item) {
+    setToDos(toDos.filter((toDo) => toDo.name != item));
+  }
+
   return (
-    <div className="w-190 mx-auto p-6 shadow-md shadow-[rgba(0,0,0,0.1)]">
+    <div className="maxk-190 mx-auto p-6 shadow-md shadow-[rgba(0,0,0,0.1)]">
       <div className="font-bold p-2 text-xl flex justify-between items-center">
-        <div>{item}</div>
-        <button className="bg-amber-300 px-4 py-2 rounded-md cursor-pointer">
+        <div>
+          <span
+            className={item.done ? `line-through` : ""}
+            onClick={() => handleClick(item.name)}
+          >
+            {item.name}
+          </span>
+        </div>
+        <button
+          onClick={() => handleDelete(item.name)}
+          className="bg-amber-300 px-4 py-2 rounded-md cursor-pointer"
+        >
           x
         </button>
       </div>
