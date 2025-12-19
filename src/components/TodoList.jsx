01417636b8a95e42fe30d9b838d641071dd30524
RenZz726 <rensonkroji02@gmail.com>
2025-12-19 11:10:51 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,15 @@
-import  TodoItem  from "./TodoItem";
-export default function TodoList({ toDos }) {
+import TodoItem from "./TodoItem";
+export default function TodoList({ toDos, setToDos }) {
+  const sortedToDos = toDos.sort((a, b) => Number(a.done) - Number(b.done))
   return (
     <div>
       {toDos.map((item) => (
-        <TodoItem key={item} item={item} />
+        <TodoItem
+          key={item.name}
+          item={item}
+          toDos={toDos}
+          setToDos={setToDos}
+        />
       ))}
     </div>
   );
