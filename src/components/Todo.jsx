0bde96f646672113a0d4dd3ee85e8af8a4749af9
RenZz726 <rensonkroji02@gmail.com>
2025-12-19 11:10:10 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import TodoList from "./TodoList";
 import Form from "./Form";
+import Footer from "./Footer";
 export default function Todo() {
   const [toDos, setTodos] = useState([]);
+  const totalToDos = toDos.length;
+  const completedToDos = toDos.filter((todo) => todo.done).length;
 
   return (
     <div>
       <Form toDos={toDos} setTodos={setTodos} />
-      <TodoList toDos={toDos} />
+      <TodoList toDos={toDos} setToDos={setTodos} />
+      <Footer completedToDos={completedToDos} totalToDos={totalToDos}/>
     </div>
   );
 }
